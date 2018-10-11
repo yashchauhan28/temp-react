5f8bc7933794ecaa3bf0065dd7da86813ef561f3
Yash Chauhan <yash.chauhan@walmartlabs.com>
2018-10-11 12:06:17 +0530

    Updates

---
+++ precache-manifest.8dc709099065ab1370aa561721baab24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4799bb15084c100a732",
+    "url": "/temp-react/static/js/runtime~main.c4799bb1.js"
+  },
+  {
+    "revision": "5ff988a2d1078689be7d",
+    "url": "/temp-react/static/js/main.5ff988a2.chunk.js"
+  },
+  {
+    "revision": "7e802ebce41dd84e0e5e",
+    "url": "/temp-react/static/js/1.7e802ebc.chunk.js"
+  },
+  {
+    "revision": "5ff988a2d1078689be7d",
+    "url": "/temp-react/static/css/main.7351c06f.chunk.css"
+  },
+  {
+    "revision": "178801896ebb6eec85241f90595a1abf",
+    "url": "/temp-react/index.html"
+  }
+];
